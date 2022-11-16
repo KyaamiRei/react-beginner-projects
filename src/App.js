45e8f16a3cef13f1cc101c6275419c7d0270e1de
kyaami <kolyavasilenko2703@gmail.com>
2022-11-16 21:43:41 +0300

    init inviteUsers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import './index.scss';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+
+import './index.scss';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [addUsers, setAddUsers] = useState([]);
+  const [successAdd, setSuccessAdd] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClickAddUser = (userId) => {
+    if (addUsers.includes(userId)) {
+      setAddUsers((prev) => prev.filter((_id) => _id !== userId));
+    } else {
+      setAddUsers((prev) => [...prev, userId]);
+    }
+    console.log(addUsers);
+  };
+
+  const onClickInvite = () => {
+    setSuccessAdd(true)
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+
+        const usersList = await axios.get('https://reqres.in/api/users');
+        setUsers(usersList.data.data);
+
+        setIsLoading(false);
+      } catch (error) {
+        alert('Ошибка при запросе пользователей');
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
+    <div className='App'>
+      {successAdd ? (
+        <Success count={addUsers.length}/>
+      ) : (
+        <Users
+          usersList={users}
+          addedUsers={addUsers.length}
+          onAddUser={(id) => onClickAddUser(id)}
+          onClickInvite={onClickInvite}
+          isLoading={isLoading}
+        />
+      )}
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import { useState } from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({ addedUsers, usersList, isLoading, onAddUser, onClickInvite }) => {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <>
-      <div className="search">
-        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+      <div className='search'>
+        <svg
+          viewBox='0 0 20 20'
+          xmlns='http://www.w3.org/2000/svg'>
+          <path d='M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z' />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          type='text'
+          placeholder='Найти пользователя...'
+        />
       </div>
       {isLoading ? (
-        <div className="skeleton-list">
+        <div className='skeleton-list'>
           <Skeleton />
           <Skeleton />
           <Skeleton />
         </div>
       ) : (
-        <ul className="users-list">
-          <User />
+        <ul className='users-list'>
+          {usersList
+            .filter((user) => {
+              const full_name = user.first_name + ' ' + user.last_name;
+              return full_name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map((user) => (
+              <User
+                onAddUser={(id) => onAddUser(id)}
+                key={user.id}
+                {...user}
+              />
+            ))}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {addedUsers > 0 ? (
+        <button
+          onClick={() => onClickInvite()}
+          className='send-invite-btn'>
+          Отправить приглашение
+        </button>
+      ) : null}
     </>
   );
 };
