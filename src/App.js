411a44b66053b5b046a6f588d90c8bbc5c291c66
kyaami <kolyavasilenko2703@gmail.com>
2022-11-16 22:07:00 +0300

    init Converter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import { Block } from './Block';
+
+import rates from './assets/rates.json';
+
 import './index.scss';
 
 function App() {
+  const [fromCurr, setFromCurr] = useState('RUB');
+  const [toCurr, setToCurr] = useState('RUB');
+
   return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+    <div className='App'>
+      <Block
+        value={0}
+        currency={fromCurr}
+        onChangeCurrency={setFromCurr}
+      />
+      <Block
+        value={0}
+        currency={toCurr}
+      />
     </div>
   );
 }
