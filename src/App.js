140a7b01fa7e53c48880db68cd9c2df3a85765ef
kyaami <kolyavasilenko2703@gmail.com>
2022-11-16 19:12:04 +0300

    init modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+
+import { Modal } from './components/Modal';
 import './index.scss';
 
 function App() {
@@ -18,21 +20,7 @@ function App() {
         className='open-modal-btn'>
         ✨ Открыть окно
       </button>
-      {isVisible && (
-        <div className='overlay'>
-          <div className='modal'>
-            <svg
-              onClick={onClickCloseButton}
-              height='200'
-              viewBox='0 0 200 200'
-              width='200'>
-              <title />
-              <path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
-            </svg>
-            <img src='https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif' />
-          </div>
-        </div>
-      )}
+      {isVisible && <Modal onClose={onClickCloseButton} />}
     </div>
   );
 }
